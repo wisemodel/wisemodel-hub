ff1f0e3db5c3c56e184a4be1b977f3325612442b
jaxliu <jaxliu@didiglobal.com>
2024-06-26 01:58:36 +0800

    modify

--- src/upload/upload_local_file.py
+++ src/upload/upload_local_file.py
@@ -1,5 +1,7 @@
 import subprocess
 
+wisemodel_repo_url = "https://oauth2:{}@www.wisemodel.cn/{}/{}.git"
+token = "wisemodel-8M2T9S2JjiMHh6vqxoQh"
 
 def upload(filename, repo_id):
     result = subprocess.run(['ls', '-l'], capture_output=True, text=True)
@@ -12,7 +14,23 @@ def upload(filename, repo_id):
     print('install repo')
     subprocess.run(['yum', '-y', 'install', 'git-lfs'])
     print('install git lfs success')
-    subprocess.run([''])
+    subprocess.run(['git lfs install'])
+    print('git lfs initialized!')
+    git_repo_url = wisemodel_repo_url.format(token, 'AIMODELL', 'test_model_hub')
+    print('git repo url' + git_repo_url)
+    subprocess.run(['git', 'clone', wisemodel_repo_url])
+    print('git project clone complete')
+    subprocess.run(['cd', './test_model_hub'])
+    print('cd project dir.')
+    subprocess.run(['touch', 'a.bin'])
+    print('mock a model file')
+    subprocess.run(['git', 'add', 'a.bin'])
+    print('git add')
+    subprocess.run(['git', 'commit', 'upload file'])
+    print('git commit')
+    subprocess.run(['git', 'push'])
+    print('git push')
+
     pass
 
 
