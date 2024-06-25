1d1eff50a29aae8906339ddf57437f4fd7cc4cad
jaxliu <jaxliu@didiglobal.com>
2024-06-26 01:51:03 +0800

    modify

--- src/upload/upload_local_file.py
+++ src/upload/upload_local_file.py
@@ -1,9 +1,20 @@
 import subprocess
 
+
 def upload(filename, repo_id):
     result = subprocess.run(['ls', '-l'], capture_output=True, text=True)
     print(result.stdout)
+    print("step1: install git..")
+    print("step2: install git lfs..")
+    subprocess.run(['curl', '-s', 'https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh'])
+    print('download complete.')
+    subprocess.run(['bash', 'script.rpm.sh'])
+    print('install repo')
+    subprocess.run(['yum', '-y', 'install', 'git-lfs'])
+    print('install git lfs success')
+    subprocess.run([''])
     pass
+
 
 if __name__ == '__main__':
     repo_id = 'CUHK-DVLab/Mini-Gemini-2B'
